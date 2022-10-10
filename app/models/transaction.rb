d90bb2956fbe78b940b6f0857ac5b73d20b427f1
Menanya <emmiemenz@gmail.com>
2022-10-10 16:41:02 +0100

    add migrations

--- app/models/transaction.rb
+++
@@ -1,6 +0,0 @@
-class Transaction < ApplicationRecord
-    has_many :category_transactions
-    validates :name, presence: true
-
-    validates :amount, presence: true, numericality: { only_integer: false, greater_than_or_equal_to: 0}
-end
