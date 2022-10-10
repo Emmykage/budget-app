526ba6e49c0198caf0169e2d38e03e5dc8b255f9
Menanya <emmiemenz@gmail.com>
2022-10-10 06:32:09 +0100

    styling

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :transactions
   resources :categories
   resources :users
-  root 'category#index'
+  root 'categories#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
