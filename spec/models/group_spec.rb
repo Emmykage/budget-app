c3ef5ffc84d633e93f75cec6b1f9b68be79cadeb
Menanya <emmiemenz@gmail.com>
2022-10-13 16:50:32 +0100

    test complete

---
+++ spec/models/group_spec.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Group, type: :model do
+  subject do
+    test_user = User.create(name: 'morris', id: 1)
+    Group.new(user_id: 1, name: 'office', icon: 'photo', id: 1)
+  end
+  before { subject.save }
+  describe 'Group' do
+    it 'photo should be present' do
+      subject.name = nil
+      expect(subject).to_not be_valid
+    end
+  end
+
+end
--- spec/models/purchase_spec.rb
+++ spec/models/purchase_spec.rb
@@ -4,18 +4,18 @@ require 'rails_helper'
 
 RSpec.describe Purchase, type: :model do
   subject do
-    user = User.create(name: 'morris')
-    Purchase.new(user:, name: 'pen', amount: 24)
+    test_user = User.create(name: 'morris', id: 2)
+    Purchase.new(user_id: 2, name: 'pen', amount: 24)
   end
   before { subject.save }
   describe 'name' do
     it 'name should be present' do
       subject.amount = nil
-      expect(subject).to_not be_valiid
+      expect(subject).to_not be_valid
     end
     it 'photo should be present' do
       subject.name = nil
-      expect(subject).to_not be_valiid
+      expect(subject).to_not be_valid
     end
   end
 end
